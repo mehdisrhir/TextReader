ab9e3869866764a07df275d3bfba6fbb4b937bb0
Mehdi <mehdi.srhir@outlook.com>
2018-07-01 16:11:50 +0200

    Txt reader first push

--- TextReader/Program.cs
+++ TextReader/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace TextReader
 {
     class Program
     {
+        public static void read(string path)
+        {
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            read(@"c:\data.txt");
+            Console.ReadLine();
         }
     }
 }
